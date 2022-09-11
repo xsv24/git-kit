926227155909604cf0bf8a10426afd96566dbea1
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-09-11 21:29:39 +1200

    🧪 Add args & branch tests.

--- src/args.rs
+++ src/args.rs
@@ -28,3 +28,92 @@ impl Arguments {
         Ok(contents)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use fake::{Fake, Faker};
+
+    use super::*;
+
+    #[test]
+    fn commit_message_with_both_args_are_populated() -> anyhow::Result<()> {
+        let conn = setup_db(None)?;
+
+        let args = Arguments {
+            ticket: Some(Faker.fake()),
+            message: Some(Faker.fake()),
+        };
+
+        let actual = args.commit_message("{ticket_num} {message}".into(), &conn)?;
+        let expected = format!("[{}] {}", args.ticket.unwrap(), args.message.unwrap());
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn commit_template_message_is_replaced_with_empty_str() -> anyhow::Result<()> {
+        let conn = setup_db(None)?;
+
+        let args = Arguments {
+            ticket: Some(Faker.fake()),
+            message: None,
+        };
+
+        let actual = args.commit_message("{ticket_num} {message}".into(), &conn)?;
+        let expected = format!("[{}] ", args.ticket.unwrap());
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn commit_template_ticket_num_is_replaced_with_branch_name() -> anyhow::Result<()> {
+        let name = get_branch_name().try_convert()?;
+        let branch = Branch::new(&name, None)?;
+
+        let conn = setup_db(Some(&branch))?;
+
+        let args = Arguments {
+            ticket: None,
+            message: Some(Faker.fake()),
+        };
+
+        let actual = args.commit_message("{ticket_num} {message}".into(), &conn)?;
+        let expected = format!("[{}] {}", name, args.message.unwrap());
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    fn setup_db(branch: Option<&Branch>) -> anyhow::Result<Connection> {
+        let conn = Connection::open_in_memory()?;
+
+        conn.execute(
+            "CREATE TABLE branch (
+                name TEXT NOT NULL PRIMARY KEY,
+                ticket TEXT,
+                data BLOB,
+                created TEXT NOT NULL
+            )",
+            (),
+        )?;
+
+        if let Some(branch) = branch {
+            conn.execute(
+                "INSERT INTO branch (name, ticket, data, created) VALUES (?1, ?2, ?3, ?4)",
+                (
+                    &branch.name,
+                    &branch.ticket,
+                    &branch.data,
+                    branch.created.to_rfc3339(),
+                ),
+            )?;
+        }
+
+        Ok(conn)
+    }
+}
