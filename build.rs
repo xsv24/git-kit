2cb51722651b5a32118965b4123d8fe75b4b7053
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-11-16 13:30:35 +1300

    ✨ Update the build script to copy over the config file.

--- build.rs
+++ build.rs
@@ -5,17 +5,33 @@ use std::{
 
 use directories::ProjectDirs;
 
-// Build script to copy over default templates into the binary directory.
+// Build script to copy over default templates & config into the binary directory.
 fn main() {
     if let Some(dirs) = ProjectDirs::from("dev", "xsv24", "git-kit") {
         // https://doc.rust-lang.org/cargo/reference/environment-variables.html
-        let project_root = &env::var("CARGO_MANIFEST_DIR").unwrap();
+        let project_root = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let package_version = env!("CARGO_PKG_VERSION");
+        let config_dir = dirs.config_dir();
 
-        copy_or_replace(
-            &Path::new(project_root).join("templates/"),
-            &dirs.config_dir().join("templates/"),
-        )
-        .unwrap();
+        match package_version {
+            version if version <= "0.0.8" => {
+                println!("Updating template files...");
+                // We've moved away from separate template files in favor of a single yaml file.
+                copy_or_replace(
+                    &project_root.join("templates/"),
+                    &config_dir.join("templates/"),
+                )
+                .expect("Failed to copy or update to the latest template files for git-kit")
+            }
+            _ => {
+                println!("Updating config file...");
+                copy_or_replace(
+                    &project_root.join(".git-kit.yml"),
+                    &config_dir.join(".git-kit.yml"),
+                )
+                .expect("Failed to copy or update to the latest config file for git-kit")
+            }
+        }
     }
 
     println!("cargo:rerun-if-changed=build.rs");
