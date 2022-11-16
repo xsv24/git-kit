f875bd0905014541ffab412c74f8b3123a80922f
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-11-16 14:22:54 +1300

    🧹 Update the build script & remove unneeded template files.

--- build.rs
+++ build.rs
@@ -10,28 +10,14 @@ fn main() {
     if let Some(dirs) = ProjectDirs::from("dev", "xsv24", "git-kit") {
         // https://doc.rust-lang.org/cargo/reference/environment-variables.html
         let project_root = Path::new(env!("CARGO_MANIFEST_DIR"));
-        let package_version = env!("CARGO_PKG_VERSION");
         let config_dir = dirs.config_dir();
 
-        match package_version {
-            version if version <= "0.0.8" => {
-                println!("Updating template files...");
-                // We've moved away from separate template files in favor of a single yaml file.
-                copy_or_replace(
-                    &project_root.join("templates/"),
-                    &config_dir.join("templates/"),
-                )
-                .expect("Failed to copy or update to the latest template files for git-kit")
-            }
-            _ => {
-                println!("Updating config file...");
-                copy_or_replace(
-                    &project_root.join(".git-kit.yml"),
-                    &config_dir.join(".git-kit.yml"),
-                )
-                .expect("Failed to copy or update to the latest config file for git-kit")
-            }
-        }
+        println!("Updating config file...");
+        copy_or_replace(
+            &project_root.join(".git-kit.yml"),
+            &config_dir.join(".git-kit.yml"),
+        )
+        .expect("Failed to copy or update to the latest config file for git-kit")
     }
 
     println!("cargo:rerun-if-changed=build.rs");
