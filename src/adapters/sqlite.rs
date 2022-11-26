d184a9ead22d7020390d799c7085590dc5db8893
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-11-26 14:44:41 +1300

    ✨ Add store config methods

--- src/adapters/sqlite.rs
+++ src/adapters/sqlite.rs
@@ -1,10 +1,13 @@
 use anyhow::{anyhow, Context};
 use chrono::{DateTime, Utc};
-use rusqlite::{types::Type, Connection, Row};
+use rusqlite::{types::Type, Connection, Row, Transaction};
 
-use crate::domain::{
-    self,
-    models::{Branch, Config},
+use crate::{
+    domain::{
+        self,
+        models::{Branch, Config, ConfigStatus},
+    },
+    utils::expected_path,
 };
 
 pub struct Sqlite {
@@ -14,6 +17,15 @@ pub struct Sqlite {
 impl Sqlite {
     pub fn new(connection: Connection) -> anyhow::Result<Sqlite> {
         Ok(Sqlite { connection })
+    }
+
+    pub fn transaction(&mut self) -> anyhow::Result<Transaction> {
+        let transaction = self
+            .connection
+            .transaction()
+            .context("Failed to open transaction for sqlite db.")?;
+
+        Ok(transaction)
     }
 
     pub fn close(self) -> anyhow::Result<()> {
@@ -66,6 +78,60 @@ impl domain::adapters::Store for Sqlite {
         Ok(branch)
     }
 
+    fn persist_config(&self, config: &Config) -> anyhow::Result<()> {
+        if config.key.to_string().eq("default") {
+            anyhow::bail!("Cannot override default!");
+        }
+
+        log::info!(
+            "insert or update user config '{}' path '{}'",
+            &config.key,
+            &config.path.display()
+        );
+
+        let path = &config.path.to_str().context("Invalid valid path given")?;
+
+        self.connection
+            .execute(
+                "REPLACE INTO config (key, path, status) VALUES (?1, ?2, ?3)",
+                (config.key.clone(), path, String::from(ConfigStatus::ACTIVE)),
+            )
+            .context("Failed to update config.")?;
+
+        Ok(())
+    }
+
+    fn set_active_config(&mut self, key: String) -> anyhow::Result<Config> {
+        let transaction = self.transaction()?;
+
+        let (active, inactive) = (
+            String::from(ConfigStatus::ACTIVE),
+            String::from(ConfigStatus::INACTIVE),
+        );
+
+        // Update any 'ACTIVE' config to 'INACTIVE'
+        transaction
+            .execute(
+                "UPDATE config SET status = ?1 WHERE status = ?2;",
+                (&inactive, &active),
+            )
+            .with_context(|| format!("Failed to set any '{}' config to '{}'.", inactive, active))?;
+
+        // Update the desired config to 'ACTIVE'
+        transaction
+            .execute(
+                "UPDATE config SET status = ?1 WHERE key = ?2;",
+                (&active, &key),
+            )
+            .with_context(|| format!("Failed to update config status to '{}'.", active))?;
+
+        transaction
+            .commit()
+            .context("Failed to commit transaction to update config")?;
+
+        self.get_config(Some(key))
+    }
+
     fn close(self) -> anyhow::Result<()> {
         log::info!("closing sqlite connection");
 
@@ -74,6 +140,58 @@ impl domain::adapters::Store for Sqlite {
             .map_err(|_| anyhow!("Failed to close 'git-kit' connection"))?;
 
         Ok(())
+    }
+
+    fn get_config(&self, key: Option<String>) -> anyhow::Result<Config> {
+        let config = match key {
+            Some(key) => {
+                self.connection
+                    .query_row("SELECT * FROM config WHERE key = ?1", [key], |row| {
+                        Config::try_from(row)
+                    })
+            }
+            None => self.connection.query_row(
+                "SELECT * FROM config WHERE status = ?1",
+                [String::from(ConfigStatus::ACTIVE)],
+                |row| Config::try_from(row),
+            ),
+        }?;
+
+        dbg!(&config);
+        Ok(config)
+    }
+}
+
+impl<'a> TryFrom<&Row<'a>> for Config {
+    type Error = rusqlite::Error;
+
+    fn try_from(value: &Row) -> Result<Self, Self::Error> {
+        let path = expected_path(&value.get::<_, String>(1)?).map_err(|e| {
+            log::error!("Corrupted data failed to convert to valid path, {}", e);
+            rusqlite::Error::InvalidColumnType(
+                1,
+                "Failed to convert to valid path".into(),
+                Type::Text,
+            )
+        })?;
+
+        let status = value.get::<_, String>(2)?.try_into().map_err(|e| {
+            log::error!(
+                "Corrupted data failed to convert to valid config status, {}",
+                e
+            );
+            rusqlite::Error::InvalidColumnType(
+                2,
+                "Failed to convert to 'ConfigStatus'".into(),
+                Type::Text,
+            )
+        })?;
+
+        Ok(Config {
+            key: value.get(0)?,
+            path,
+            status,
+        })
     }
 }
 
@@ -106,7 +224,10 @@ impl<'a> TryFrom<&Row<'a>> for Branch {
 
 #[cfg(test)]
 mod tests {
-    use std::{collections::HashMap, path::{PathBuf, Path}};
+    use std::{
+        collections::HashMap,
+        path::{Path, PathBuf},
+    };
 
     use crate::{
         adapters::Git,
@@ -138,6 +259,31 @@ mod tests {
         assert_eq!(branch.ticket, ticket);
         assert_eq!(branch.data, data);
         assert_eq!(branch.created.to_rfc3339(), created);
+
+        Ok(())
+    }
+
+    #[test]
+    fn update_config_creates_a_new_item_if_not_exists() -> anyhow::Result<()> {
+        // Arrange
+        let config = fake_config();
+        let connection = setup_db()?;
+        let store = Sqlite::new(connection)?;
+
+        // Act
+        store.persist_config(&config)?;
+
+        // Assert
+        assert_eq!(config_count(&store.connection)?, 1);
+
+        let (key, path) = select_config_row(&store.connection, config.key.clone())?;
+        let expected = Config {
+            key,
+            path: Path::new(&path).to_owned(),
+            status: ConfigStatus::ACTIVE,
+        };
+
+        assert_eq!(expected, config);
 
         Ok(())
     }
@@ -280,6 +426,14 @@ mod tests {
         }
     }
 
+    fn fake_config() -> Config {
+        Config {
+            key: Faker.fake(),
+            path: PathBuf::new(),
+            status: ConfigStatus::ACTIVE,
+        }
+    }
+
     fn fake_branch(name: Option<String>, repo: Option<String>) -> anyhow::Result<Branch> {
         let name = name.unwrap_or(Faker.fake());
         let repo = repo.unwrap_or(Faker.fake());
@@ -303,15 +457,36 @@ mod tests {
         Ok((name, ticket, data, created))
     }
 
+    fn select_config_row(conn: &Connection, key: String) -> anyhow::Result<(String, String)> {
+        let path = conn.query_row("SELECT * FROM config where key = ?1;", [key], |row| {
+            Ok((row.get::<_, String>(0)?, row.get::<_, String>(1)?))
+        })?;
+
+        Ok(path)
+    }
+
     fn branch_count(conn: &Connection) -> anyhow::Result<i32> {
         let count: i32 = conn.query_row("SELECT COUNT(*) FROM branch", [], |row| row.get(0))?;
 
         Ok(count)
     }
 
+    fn config_count(conn: &Connection) -> anyhow::Result<i32> {
+        let count: i32 = conn.query_row("SELECT COUNT(*) FROM config", [], |row| row.get(0))?;
+
+        Ok(count)
+    }
+
     fn setup_db() -> anyhow::Result<Connection> {
         let mut conn = Connection::open_in_memory()?;
-        db_migrations(&mut conn, None)?;
+        db_migrations(
+            &mut conn,
+            migrations::MigrationContext {
+                config_path: Path::new(".").to_owned(),
+                enable_side_effects: false,
+                version: None,
+            },
+        )?;
         Ok(conn)
     }
 }
--- src/domain/adapters/store.rs
+++ src/domain/adapters/store.rs
@@ -1,9 +1,15 @@
-use crate::domain::models::Branch;
+use crate::domain::models::{Branch, Config};
 
 pub trait Store {
     fn persist_branch(&self, branch: &Branch) -> anyhow::Result<()>;
 
     fn get_branch(&self, branch: &str, repo: &str) -> anyhow::Result<Branch>;
+
+    fn persist_config(&self, config: &Config) -> anyhow::Result<()>;
+
+    fn set_active_config(&mut self, key: String) -> anyhow::Result<Config>;
+
+    fn get_config(&self, key: Option<String>) -> anyhow::Result<Config>;
 
     fn close(self) -> anyhow::Result<()>;
 }
