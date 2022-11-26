78a73d11209377947a3369e14787dccdb5818c1f
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-11-26 14:52:34 +1300

    ✨ Add try_convert methods for Path.

--- src/utils/try_convert.rs
+++ src/utils/try_convert.rs
@@ -1,4 +1,6 @@
-use std::process::Command;
+use std::{path::PathBuf, process::Command};
+
+use anyhow::Context;
 
 // Implement our own try_into just as a work around to implement
 // TryInto for external types outside our crate.
@@ -12,5 +14,20 @@ impl TryConvert<String> for Command {
         let stdout = String::from_utf8_lossy(&output.stdout);
 
         Ok(stdout.into())
+    }
+}
+
+impl TryConvert<String> for PathBuf {
+    fn try_convert(self) -> anyhow::Result<String> {
+        let path = self
+            .to_str()
+            .context("Failed to convert path into string")?;
+        Ok(path.into())
+    }
+}
+
+impl TryConvert<String> for &PathBuf {
+    fn try_convert(self) -> anyhow::Result<String> {
+        self.to_owned().try_convert()
     }
 }
